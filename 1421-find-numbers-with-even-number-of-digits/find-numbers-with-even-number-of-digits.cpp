7e1d6f3f5456ab5c4d441e167b2e7117e8658515
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2025-03-10 20:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (90.41%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        
+        for (int num : nums) {
+            int digitCount = 0;
+            int temp = num;
+            
+            // Count the number of digits
+            while (temp != 0) {
+                digitCount++;
+                temp /= 10;
+            }
+            
+            // Check if the number of digits is even
+            if (digitCount % 2 == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
