1e53e8d91af633c160885d5b53c3a72a08aba589
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-07-30 22:37:20 +0530

    ✅ Minimum Number of Pushes to Type Word I | Easy | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-07-30

--- easy/3014-minimum-number-of-pushes-to-type-word-i/solution.py
+++ easy/3014-minimum-number-of-pushes-to-type-word-i/solution.py
@@ -1,6 +1,6 @@
-class Solution:
-    def minimumPushes(self, word: str) -> int:
-        cost = 0
-        for i in range(len(word)):
-            cost += (i//8)+1
-        return cost
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        cost = 0
+        for i in range(len(word)):
+            cost += (i//8)+1
+        return cost
