3238c9848a06ea0248c72ababb268987ae624b1d
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-04 21:36:33 +0530

    ✅ Find Missing Elements | Easy | Python | Runtime: 7 ms (24.45%) | Memory: 19.3 MB | 2026-08-04

--- easy/3731-find-missing-elements/solution.py
+++ easy/3731-find-missing-elements/solution.py
@@ -1,8 +1,8 @@
-class Solution:
-    def findMissingElements(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        missing = []
-        for i in range(nums[0],nums[-1]+1):
-            if i not in nums:
-                missing.append(i)
-        return missing
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        missing = []
+        for i in range(nums[0],nums[-1]+1):
+            if i not in nums:
+                missing.append(i)
+        return missing
