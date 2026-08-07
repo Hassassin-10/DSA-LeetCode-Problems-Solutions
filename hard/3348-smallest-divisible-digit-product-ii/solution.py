eece7343e5dbf745249f13720fcf2f27cbd128da
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-07 18:49:10 +0530

    ✅ Smallest Divisible Digit Product II | Hard | Python | Runtime: 0 ms | Memory: 19.6 MB | 2026-08-07

---
+++ hard/3348-smallest-divisible-digit-product-ii/solution.py
@@ -0,0 +1,21 @@
+            rem[i + 1] = rem[i] // math.gcd(rem[i], int(num_list[i]))
+                break
+                pos = i
+            if num_list[i] == "0":
+        num_list = list(num)
+        for i in range(n):
+
+        pos = n - 1
+        rem[0] = t
+        rem = [0] * (n + 1)
+
+        n = len(num)
+            return "-1"
+        if temp > 1:
+
+                temp //= i
+            while temp % i == 0:
+        for i in range(2, 10):
+        temp = t
+    def smallestNumber(self, num: str, t: int) -> str:
+class Solution:
