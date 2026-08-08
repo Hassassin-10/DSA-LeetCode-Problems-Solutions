aadb227a77592f30310433120efee6f0b3e1ef7c
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-08 11:47:14 +0530

    ✅ Find the Lexicographically Smallest Valid Sequence | Medium | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-08-08

--- medium/3302-find-the-lexicographically-smallest-valid-sequence/solution.py
+++ medium/3302-find-the-lexicographically-smallest-valid-sequence/solution.py
@@ -1,17 +1,20 @@
-                break
-            nxt[j] = p
-            p -= 1
-
-        ans, p, used = [], -1, False
-
-        for j in range(m):
-            for i in range(p + 1, nxt[j + 1]):
-                if word1[i] == word2[j] or not used:
-                    ans.append(i)
-                    used |= word1[i] != word2[j]
-                    p = i
+                if right[j + 1] <= i:
                     break
-            else:
+
+                if word1[i] == word2[j]:
+                    ans.append(i)
+                    p = i
+                    found = True
+                    break
+
+                if not used:
+                    ans.append(i)
+                    p = i
+                    used = True
+                    found = True
+                    break
+
+            if not found:
                 return []
 
         return ans
