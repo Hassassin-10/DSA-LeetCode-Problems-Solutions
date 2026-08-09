f121bf8eef7a3404c02eb35a845cf09985dd9c4b
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-09 20:43:01 +0530

    ✅ Stone Game II | Medium | Python | Runtime: 183 ms (50.95%) | Memory: 27.2 MB | 2026-08-09

--- medium/1140-stone-game-ii/solution.py
+++ medium/1140-stone-game-ii/solution.py
@@ -1,20 +1,39 @@
-            # Current player can take X piles.
-            # After that, the opponent gets dp(next_i, next_m).
-            # Therefore current player's final amount is:
-            #
-            # total remaining - opponent's best amount
-            best = 0
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
 
-            for x in range(1, min(2 * m, n - i) + 1):
-                next_i = i + x
-                next_m = max(m, x)
+        # suffix[i] = total stones from i to the end
+        suffix = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
 
-                best = max(
-                    best,
-                    suffix[i] - dp(next_i, next_m)
-                )
+        memo = {}
 
-            memo[(i, m)] = best
-            return best
+        def dp(i, m):
+            # No piles left
+            if i == n:
+                return 0
 
-        return dp(0, 1)
+            if (i, m) in memo:
+                return memo[(i, m)]
+
+            # Current player can take X piles.
+            # After that, the opponent gets dp(next_i, next_m).
+            # Therefore current player's final amount is:
+            #
+            # total remaining - opponent's best amount
+            best = 0
+
+            for x in range(1, min(2 * m, n - i) + 1):
+                next_i = i + x
+                next_m = max(m, x)
+
+                best = max(
+                    best,
+                    suffix[i] - dp(next_i, next_m)
+                )
+
+            memo[(i, m)] = best
+            return best
+
+        return dp(0, 1)
