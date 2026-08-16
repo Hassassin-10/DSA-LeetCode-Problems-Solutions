a7c201551638ba52479c3e82e1fd452854d3bad6
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-16 10:24:27 +0530

    ✅ Stone Game IX | Medium | Python | Runtime: 40 ms (96.18%) | Memory: 30.3 MB | 2026-08-16

--- medium/2029-stone-game-ix/solution.py
+++ medium/2029-stone-game-ix/solution.py
@@ -1,14 +1,14 @@
-class Solution:
-    def stoneGameIX(self, stones: list[int]) -> bool:
-        cnt = [0, 0, 0]
+class Solution:
+    def stoneGameIX(self, stones: list[int]) -> bool:
+        cnt = [0, 0, 0]
 
-        for x in stones:
-            cnt[x % 3] += 1
+        for x in stones:
+            cnt[x % 3] += 1
 
-        # Stones divisible by 3 don't change the sum modulo 3.
-        # They can effectively be used as extra moves.
-        if cnt[0] % 2 == 0:
-            return cnt[1] > 0 and cnt[2] > 0
+        # Stones divisible by 3 don't change the sum modulo 3.
+        # They can effectively be used as extra moves.
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
 
-        # Odd number of 0-modulo stones
-        return abs(cnt[1] - cnt[2]) > 2
+        # Odd number of 0-modulo stones
+        return abs(cnt[1] - cnt[2]) > 2
