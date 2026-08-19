e491f14231f6515db1dbdfad6629cacb99a69d0f
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-19 21:07:56 +0530

    ✅ Cinema Seat Allocation | Medium | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-08-19

---
+++ medium/1386-cinema-seat-allocation/solution.py
@@ -0,0 +1,20 @@
+
+        for r, s in reservedSeats:
+            rows[r] = rows.get(r, 0) | (1 << s)
+
+        # Every completely free row can accommodate 2 groups.
+        ans = (n - len(rows)) * 2
+
+        for mask in rows.values():
+            left = (mask & ((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5))) == 0
+            middle = (mask & ((1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))) == 0
+            right = (mask & ((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9))) == 0
+
+            if left and right:
+                # Both non-overlapping blocks can be used.
+                ans += 2
+            elif left or middle or right:
+                # At least one valid block exists.
+                ans += 1
+
+        return ans
