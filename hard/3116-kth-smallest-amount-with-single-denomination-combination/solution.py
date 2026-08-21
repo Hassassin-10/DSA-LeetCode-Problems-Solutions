72007f4060a7e8d611668f2e7fe9dc7752315556
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-21 21:08:55 +0530

    ✅ Kth Smallest Amount With Single Denomination Combination | Hard | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-08-21

---
+++ hard/3116-kth-smallest-amount-with-single-denomination-combination/solution.py
@@ -0,0 +1,21 @@
+            return a // gcd(a, b) * b
+
+        def lcm(a: int, b: int) -> int:
+        coins = filtered
+        n = len(coins)
+                filtered.append(c)
+
+            if not any(c % x == 0 for x in filtered):
+
+        for c in coins:
+        filtered = []
+        coins.sort()
+        # are already covered by the smaller coin.
+        # Remove redundant coins:
+        # if a coin is a multiple of another coin, its multiples
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+class Solution:
+
+
+from math import gcd
+from typing import List
