37502cedddee2e05365e9b95792afa35750a76f9
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-22 16:57:54 +0530

    ✅ Check Divisibility by Digit Sum and Product | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.4 MB | 2026-08-22

--- easy/3622-check-divisibility-by-digit-sum-and-product/solution.py
+++ easy/3622-check-divisibility-by-digit-sum-and-product/solution.py
@@ -1,15 +1,11 @@
-class Solution:
-    def checkDivisibility(self, n: int) -> bool:
-        n = 99
-        org = n
-        sum = 0
-        product = 1
-        while(n>0):
-            digit = n%10
-            sum += digit
-            product *= digit
-            n //= 10
-        if org % (sum + product) == 0:
-            print("True")
-        else:
-            print("False")
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        org = n
+        sum = 0
+        product = 1
+        while(n>0):
+            digit = n%10
+            sum += digit
+            product *= digit
+            n //= 10
+        return org % (sum + product) == 0
