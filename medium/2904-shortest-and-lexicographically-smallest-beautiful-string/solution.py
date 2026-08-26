730fa8a84d711879fce417abee11ce26fac8b0b0
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-26 12:22:29 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Python | Runtime: 3 ms (53.15%) | Memory: 19.2 MB | 2026-08-26

--- medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.py
+++ medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.py
@@ -1,20 +1,31 @@
-            while ones > k:
-                if s[left] == '1':
-                    ones -= 1
-                left += 1
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        left = 0
+        ones = 0
+        best = ""
 
-            # We have exactly k ones.
-            if ones == k:
-                # Remove leading zeros.
-                while s[left] == '0':
-                    left += 1
+        for right in range(len(s)):
+            if s[right] == '1':
+                ones += 1
 
-                curr = s[left:right + 1]
+            # We have more than k ones, so move left.
+            while ones > k:
+                if s[left] == '1':
+                    ones -= 1
+                left += 1
 
-                # Choose shortest, then lexicographically smallest.
-                if (not best or
-                    len(curr) < len(best) or
-                    (len(curr) == len(best) and curr < best)):
-                    best = curr
+            # We have exactly k ones.
+            if ones == k:
+                # Remove leading zeros.
+                while s[left] == '0':
+                    left += 1
 
-        return best
+                curr = s[left:right + 1]
+
+                # Choose shortest, then lexicographically smallest.
+                if (not best or
+                    len(curr) < len(best) or
+                    (len(curr) == len(best) and curr < best)):
+                    best = curr
+
+        return best
