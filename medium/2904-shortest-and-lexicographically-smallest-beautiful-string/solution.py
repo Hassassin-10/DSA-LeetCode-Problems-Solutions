1e5d2166027830c99176eb496f133de80e3325a5
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-26 12:22:26 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-08-26

---
+++ medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.py
@@ -0,0 +1,20 @@
+            while ones > k:
+                if s[left] == '1':
+                    ones -= 1
+                left += 1
+
+            # We have exactly k ones.
+            if ones == k:
+                # Remove leading zeros.
+                while s[left] == '0':
+                    left += 1
+
+                curr = s[left:right + 1]
+
+                # Choose shortest, then lexicographically smallest.
+                if (not best or
+                    len(curr) < len(best) or
+                    (len(curr) == len(best) and curr < best)):
+                    best = curr
+
+        return best
