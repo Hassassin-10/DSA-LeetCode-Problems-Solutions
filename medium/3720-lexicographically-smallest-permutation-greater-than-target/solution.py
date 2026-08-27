7d6cd570e9bca03c229b598779fdb63cc125ed91
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-27 12:55:46 +0530

    ✅ Lexicographically Smallest Permutation Greater Than Target | Medium | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-08-27

--- medium/3720-lexicographically-smallest-permutation-greater-than-target/solution.py
+++ medium/3720-lexicographically-smallest-permutation-greater-than-target/solution.py
@@ -1,47 +1,20 @@
-class Solution:
-    def lexGreaterPermutation(self, s: str, target: str) -> str:
-        n = len(s)
+                    # The prefix target[:i] has already consumed
+                    # those characters, so construct the smallest suffix.
 
-        # Frequency of characters in s
-        cnt = [0] * 26
-        for ch in s:
-            cnt[ord(ch) - ord('a')] += 1
+                    cnt[c] -= 1
 
-        # Try to make the answer greater at position i.
-        # We go from right to left so that the first difference
-        # happens as late as possible.
-        for i in range(n - 1, -1, -1):
-            # Recreate the available characters after using target[:i]
-            rem = cnt[:]
+                    prefix = target[:i] + chr(c)
+                if cnt[c] > 0:
+            for c in range(t + 1, 26):
+                    suffix = []
+                    for x in range(26):
+                        suffix.append(chr(x + ord('a')) * cnt[x])
 
-            possible = True
+                    return prefix + ''.join(suffix)
 
-            for j in range(i):
-                x = ord(target[j]) - ord('a')
+            # Consume target[i] so we can continue matching it.
+            if cnt[t] == 0:
+                break
+            cnt[t] -= 1
 
-                if rem[x] == 0:
-                    possible = False
-                    break
-
-                rem[x] -= 1
-
-            if not possible:
-                continue
-
-            # Find the smallest character > target[i]
-            x = ord(target[i]) - ord('a')
-
-            for c in range(x + 1, 26):
-                if rem[c] > 0:
-                    rem[c] -= 1
-
-                    # target[:i] + chosen larger character
-                    ans = target[:i] + chr(ord('a') + c)
-
-                    # Smallest possible suffix
-                    for k in range(26):
-                        ans += chr(ord('a') + k) * rem[k]
-
-                    return ans
-
-        return ""
+        return ""
