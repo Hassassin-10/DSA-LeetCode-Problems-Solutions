e2a87c678a4fe5255f360b29aaa8a0f1da2db0de
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-29 21:52:12 +0530

    ✅ Make Lexicographically Smallest Array by Swapping Elements | Medium | Python | Runtime: 344 ms (35.07%) | Memory: 46.7 MB | 2026-08-29

--- medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.py
+++ medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.py
@@ -1,17 +1,27 @@
+from typing import List
 
-        for end in range(1, n + 1):
-            # End the current connected group if there is a large gap.
-            if end == n or arr[end][0] - arr[end - 1][0] > limit:
-                # Values in this group are already sorted.
-                values = [arr[i][0] for i in range(start, end)]
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        n = len(nums)
 
-                # These values can be assigned to any indices in the group.
-                indices = sorted(arr[i][1] for i in range(start, end))
+        # (value, original index), sorted by value
+        arr = sorted((value, i) for i, value in enumerate(nums))
 
-                for idx, value in zip(indices, values):
-                    ans[idx] = value
+        ans = nums[:]
+        start = 0
 
-                start = end
+        for end in range(1, n + 1):
+            # End the current connected group if there is a large gap.
+            if end == n or arr[end][0] - arr[end - 1][0] > limit:
+                # Values in this group are already sorted.
+                values = [arr[i][0] for i in range(start, end)]
 
-        return ans
+                # These values can be assigned to any indices in the group.
+                indices = sorted(arr[i][1] for i in range(start, end))
 
+                for idx, value in zip(indices, values):
+                    ans[idx] = value
+
+                start = end
+
+        return ans
