3632a9d60d4fcdf6afcd3cdf409bd0a23b965cb4
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-08-31 15:08:42 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | Python | Runtime: 82 ms (55.16%) | Memory: 62.9 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.py
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.py
@@ -1,20 +1,37 @@
-            if is_max or is_min:
-                if first == -1:
-                    first = pos
-                else:
-                    min_dist = min(min_dist, pos - last)
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        prev = head
+        curr = head.next
+        pos = 1
 
-                last = pos
+        first = -1
+        last = -1
+        min_dist = float('inf')
 
-            prev = curr
-            curr = next_node
+        while curr.next:
+            next_node = curr.next
+            pos += 1
 
-        # Fewer than two critical points
-        if first == last:
-            return [-1, -1]
+            # Check if curr is a local maximum or minimum
+            is_max = curr.val > prev.val and curr.val > next_node.val
+            is_min = curr.val < prev.val and curr.val < next_node.val
 
-        # Distance between first and last critical points
-        max_dist = last - first
+            if is_max or is_min:
+                if first == -1:
+                    first = pos
+                else:
+                    min_dist = min(min_dist, pos - last)
 
-        return [min_dist, max_dist]
+                last = pos
 
+            prev = curr
+            curr = next_node
+
+        # Fewer than two critical points
+        if first == last:
+            return [-1, -1]
+
+        # Distance between first and last critical points
+        max_dist = last - first
+
+        return [min_dist, max_dist]
