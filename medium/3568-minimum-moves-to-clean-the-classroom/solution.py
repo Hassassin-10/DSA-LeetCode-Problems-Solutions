a17f6250520a4ed10d107772578438e155631f34
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-09-01 21:31:29 +0530

    ✅ Minimum Moves to Clean the Classroom | Medium | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-09-01

---
+++ medium/3568-minimum-moves-to-clean-the-classroom/solution.py
@@ -0,0 +1,22 @@
+                if en == 0:
+                # (If we're on R, entering it would already have reset it.)
+                # Cannot make another move without energy.
+
+                r, c = divmod(pos, n)
+
+                    return moves
+                if mask == full_mask:
+
+                pos, mask, en = q.popleft()
+            for _ in range(len(q)):
+        while q:
+
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        moves = 0
+        q.append((start, 0, energy))
+        # Store distance separately by BFS layers.
+
+                    continue
+
+                for dr, dc in directions:
