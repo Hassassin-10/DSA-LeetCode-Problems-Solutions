98f1d3619765665377144d89cceb4735bb4b45be
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-09-03 19:49:14 +0530

    ✅ Construct Uniform Parity Array II | Medium | Python | Runtime: 79 ms (46.34%) | Memory: 35.3 MB | 2026-09-03

--- medium/3876-construct-uniform-parity-array-ii/solution.py
+++ medium/3876-construct-uniform-parity-array-ii/solution.py
@@ -1,20 +1,19 @@
-class Solution:
-    def uniformArray(self, nums1: list[int]) -> bool:
-        min_odd = float('inf')
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        min_odd = float('inf')
 
-        # Find the smallest odd number.
-        for x in nums1:
-            if x % 2 == 1:
-                min_odd = min(min_odd, x)
+        # Find the smallest odd number.
+        for x in nums1:
+            if x % 2 == 1:
+                min_odd = min(min_odd, x)
 
-        # If there are no odd numbers, all numbers are even.
-        if min_odd == float('inf'):
-            return True
+        # If there are no odd numbers, all numbers are even.
+        if min_odd == float('inf'):
+            return True
 
-        # An even number smaller than min_odd makes it impossible.
-        for x in nums1:
-            if x % 2 == 0 and x < min_odd:
-                return False
+        # An even number smaller than min_odd makes it impossible.
+        for x in nums1:
+            if x % 2 == 0 and x < min_odd:
+                return False
 
-        return True
-
+        return True
