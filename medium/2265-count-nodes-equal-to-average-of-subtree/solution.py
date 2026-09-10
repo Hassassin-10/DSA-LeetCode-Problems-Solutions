5c8d1342ecaaf0db3a30d2a17bd96636ae82aff8
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-09-10 18:24:24 +0530

    ✅ Count Nodes Equal to Average of Subtree | Medium | Python | Runtime: 44 ms (90.50%) | Memory: 19.7 MB | 2026-09-10

--- medium/2265-count-nodes-equal-to-average-of-subtree/solution.py
+++ medium/2265-count-nodes-equal-to-average-of-subtree/solution.py
@@ -1,19 +1,29 @@
-            nonlocal ans
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfSubtree(self, root):
+        ans = 0
 
-            if not node:
-                return 0, 0
+        def dfs(node):
+            nonlocal ans
 
-            left_sum, left_count = dfs(node.left)
-            right_sum, right_count = dfs(node.right)
+            if not node:
+                return 0, 0
 
-            total_sum = left_sum + right_sum + node.val
-            total_count = left_count + right_count + 1
+            left_sum, left_count = dfs(node.left)
+            right_sum, right_count = dfs(node.right)
 
-            if total_sum // total_count == node.val:
-                ans += 1
+            total_sum = left_sum + right_sum + node.val
+            total_count = left_count + right_count + 1
 
-            return total_sum, total_count
+            if total_sum // total_count == node.val:
+                ans += 1
 
-        dfs(root)
-        return ans
+            return total_sum, total_count
 
+        dfs(root)
+        return ans
