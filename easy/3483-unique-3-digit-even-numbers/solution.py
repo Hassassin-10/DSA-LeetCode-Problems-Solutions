952948078996d7a4f1c3eaf99b86597167b2dd15
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-09-11 23:07:38 +0530

    ✅ Unique 3-Digit Even Numbers | Easy | Python | Runtime: 141 ms (15.16%) | Memory: 19.1 MB | 2026-09-11

--- easy/3483-unique-3-digit-even-numbers/solution.py
+++ easy/3483-unique-3-digit-even-numbers/solution.py
@@ -1,20 +1,30 @@
-            tens = (num // 10) % 10
-            hundreds = num // 100
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        freq = [0] * 10
 
-            need = [0] * 10
-            need[ones] += 1
-            need[tens] += 1
-            need[hundreds] += 1
+        for d in digits:
+            freq[d] += 1
 
-            possible = True
+        ans = 0
 
-            for d in range(10):
-                if need[d] > freq[d]:
-                    possible = False
-                    break
+        for num in range(100, 1000, 2):
+            ones = num % 10
+            tens = (num // 10) % 10
+            hundreds = num // 100
 
-            if possible:
-                ans += 1
+            need = [0] * 10
+            need[ones] += 1
+            need[tens] += 1
+            need[hundreds] += 1
 
-        return ans
+            possible = True
 
+            for d in range(10):
+                if need[d] > freq[d]:
+                    possible = False
+                    break
+
+            if possible:
+                ans += 1
+
+        return ans
