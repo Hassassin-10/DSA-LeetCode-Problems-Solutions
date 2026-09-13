3c4cdde67db169850ddc82e3ce56fe6b1f46a620
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2026-09-13 12:14:23 +0530

    ✅ Image Overlap | Medium | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-09-13

---
+++ medium/0835-image-overlap/solution.py
@@ -0,0 +1,20 @@
+        ones2 = []
+
+        for r in range(n):
+            for c in range(n):
+                if img1[r][c] == 1:
+                    ones1.append((r, c))
+                if img2[r][c] == 1:
+                    ones2.append((r, c))
+
+        shifts = Counter()
+
+        # If img1's (r1, c1) is moved to img2's (r2, c2),
+        # the required translation is (r2-r1, c2-c1).
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                shift = (r2 - r1, c2 - c1)
+                shifts[shift] += 1
+
+        return max(shifts.values(), default=0)
+
